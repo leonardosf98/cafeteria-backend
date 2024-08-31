4a086e0e5d2d862c6c804c81c5e8fa0722c4834f
Leonardo Souza <leonardo-f98@hotmail.com>
2024-08-31 07:50:56 -0300

    initial commit - Spring Boot project setup

---
+++ src/main/kotlin/com/example/cafeteria_backend/CafeteriaBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cafeteria_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CafeteriaBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<CafeteriaBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/cafeteria_backend/CafeteriaBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.cafeteria_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CafeteriaBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
